0c064d9665fdfbf480d9e3281d4757920d6ac1be
REDFOX39 <daniil.fox1998@yandex.ru>
2021-10-15 16:00:31 +0300

    Задача 2

--- script.js
+++ script.js
@@ -8,10 +8,15 @@ let nums = [1,2,3,4,5,6,7,8,9,10,762367,45,28,666,39];
 // nums.reduceRight((accumulator, element) => accumulator + Math.sqrt(element), 0);
 for (let i = 0; i < nums.length; i++) {
   let newElement = document.createElement('li');
-  newElement.classList.add("list__item");
+  newElement.classList.add('list__item');
   let b = i + 1;
-  newElement.classList.add("list__item-" + b);
-  console.log(nums[i]);
+  newElement.classList.add('list__item-' + b);
+  // console.log(nums[i]);
   newElement.textContent = nums[i];
 	arrayList.append(newElement)
+}
+
+for (let i = 1; i <= 10; i++) {
+  let sharp = '#'.repeat(i);
+  console.log(sharp);
 }
